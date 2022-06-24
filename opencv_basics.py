16f134abcd1945c17a229cf09ac00394caa76f4d
Karthik Banjan <92287364+karthikbanjan@users.noreply.github.com>
2022-06-24 10:39:28 +0530

    Update opencv_basics.py

--- opencv_basics.py
+++ opencv_basics.py
@@ -8,6 +8,35 @@ def rescale(img, scale):
     return cv.resize(img, None, fx=scale, fy=scale)
 
 
+# Function to display image with scaling
+def display_img(img, scale=1.0):
+    img = rescale(img, scale)
+    cv.imshow("Image", img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+# Function to display video with scaling
+def display_video(vid_path, vid_title, scale=1.0):
+    cap = cv.VideoCapture(vid_path)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            cap.release()
+            cv.destroyAllWindows()
+            return
+
+        frame = rescale(frame, scale)
+        cv.imshow(vid_title, frame)
+
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
+
+
 while True:
     print()
     print("1. WebCam Capture")
@@ -18,27 +47,12 @@ while True:
     choice = int(input("Enter your choice: "))
 
     if choice == 1:
-        cap = cv.VideoCapture(0)
-        while True:
-            ret, frame = cap.read()
-            cv.imshow("WebCam", frame)
-            if cv.waitKey(1) & 0xFF == ord('q'):
-                break
-        cap.release()
-        cv.destroyAllWindows()
+        display_video(0, "Webcam")
 
     elif choice == 2:
         video_path = input("Please provide the video path: ")
         cap = cv.VideoCapture(video_path)
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            cv.imshow("Video", frame)
-            if cv.waitKey(1) & 0xFF == ord('q'):
-                break
-        cap.release()
-        cv.destroyAllWindows()
+        display_video(video_path, "Original Video")
 
         print()
         print("Do you wish to rescale the video?")
@@ -47,27 +61,16 @@ while True:
         choice = int(input("Enter your choice: "))
         if choice == 1:
             cap = cv.VideoCapture(video_path)
-            scale = float(input("Enter the scale: "))
-            while True:
-                ret, frame = cap.read()
-                if not ret:
-                    break
-                frame = rescale(frame, scale)
-                cv.imshow("Video", frame)
-                if cv.waitKey(1) & 0xFF == ord('q'):
-                    break
-            cap.release()
-            cv.destroyAllWindows()
+            scale = float(input("Enter the scale factor: "))
+            display_video(video_path, "Scaled Video", scale)
 
         elif choice == 2:
-            print("Video rescaling not required")
+            print("Video rescaling not required.")
 
     elif choice == 3:
         img_path = input("Please provide the image path: ")
         img = cv.imread(img_path)
-        cv.imshow("Image", img)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
+        display_img(img)
 
         print()
         print("Do you wish to display the image in different sizes?")
@@ -75,15 +78,12 @@ while True:
         print("2. No")
         choice = int(input("Enter your choice: "))
         if choice == 1:
-            print("Enter the scale factor: ")
-            scale = float(input())
-            img = rescale(img, scale)
-            cv.imshow("Image", img)
-            cv.waitKey(0)
-            cv.destroyAllWindows()
+            scale = float(input("Enter the scale factor: "))
+            display_img(img, scale)
 
         elif choice == 2:
-            print("Image rescaling not required")
+            print("Image rescaling not required.")
 
     elif choice == 4:
+        print("Thank you for using this program!")
         break
